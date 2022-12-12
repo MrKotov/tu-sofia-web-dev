2b24d4770211a086fe266f9fb2bed70b6aef50bd
gkotov <georgi.kotov@dss.bg>
2022-12-12 07:24:27 +0200

    feat(hero-table): add hero frontend table

--- lerning-fontend-with-angular/src/app/hero/hero-list/hero-list.component.ts
+++ lerning-fontend-with-angular/src/app/hero/hero-list/hero-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero.model';
+import { Hero, Power } from '../hero.model';
 import { HeroService } from '../services/hero.service';
 
 @Component({
@@ -9,15 +9,45 @@ import { HeroService } from '../services/hero.service';
 })
 export class HeroListComponent implements OnInit {
   public heroes: any;
+  public heroDialog: boolean = false;
+  public hero: Hero = {} as Hero;
 
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes() {
     this.heroService.getAllHeroes().subscribe({
-      next: (heroes) => {
-        this.heroes = heroes;
+      next: (heroes: Hero[]) => {
+        let parsedHeroes = heroes.map((h: Hero) => {
+          return { ...h, powers: h.powers.map((p: Power) => p.name) };
+        });
+        this.heroes = parsedHeroes;
       },
       error: (err) => {},
+    });
+  }
+
+  showDialog() {
+    this.heroDialog = true;
+  }
+
+  hideDialog() {
+    this.hero = {} as Hero;
+  }
+
+  createHero() {
+    this.heroService.createHero(this.hero).subscribe({
+      next: (hero) => {
+        this.getHeroes();
+        this.hero = {} as Hero;
+        this.heroDialog = false;
+      },
+      error: (err) => {
+        console.error(err);
+      },
     });
   }
 }
